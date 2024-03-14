8774c389c656a08a01a6881d4b14a8e39d8bed53
annagolod <anna.golod@gmail.com>
2024-03-14 21:17:29 +0200

    added SuccessLoginTests

--- src/main/java/manager/HelperUser.java
+++ src/main/java/manager/HelperUser.java
@@ -1,9 +1,34 @@
 package manager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 public class HelperUser extends HelperBase{
     public HelperUser(WebDriver wd) {
         super(wd);
     }
+
+    public void openLoginForm(){
+        click(By.xpath("//a[@href='/login?url=%2Fsearch']"));
+    }
+
+    public void fillLoginForm(String email, String password){
+        type(By.id("email"), email);
+        type(By.id("password"), password);
+    }
+
+    public void submitLogin(){
+        click(By.cssSelector("button[type = 'submit']"));
+    }
+
+    public boolean isLogged(){
+        return isElementPresent(By.xpath("//h2[text() = 'Logged in success']"));
+    }
+    public void logout(){
+        click(By.cssSelector("a[href='/logout?url=%2Fsearch']"));
+    }
+
+
+
+
 }
--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,11 +1,69 @@
 package tests;
 
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class LoginTests extends TestBase{
 
+//    @BeforeMethod
+//    public void preCondition(){
+//        if(app.getHelperUser().isLogged()){
+//            app.getHelperUser().logout();
+//        }
+//    }
+
+    @AfterMethod
+    public void postCondition(){
+        if(app.getHelperUser().isLogged()) {
+            app.getHelperUser().click(By.xpath("//button[text() = 'Ok']"));
+            app.getHelperUser().logout();
+        } else {
+            app.getHelperUser().click(By.xpath("//button[text() = 'Ok']"));
+        }
+    }
+
     @Test
-    public void test(){
+    public void loginPositiveTest(){
+       app.getHelperUser().openLoginForm();
+       app.getHelperUser().fillLoginForm("tretam0810@gmail.com", "Carro54321#");
+       app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+        //app.getHelperUser().click(By.xpath("//button[text() = 'Ok']"));
+
+
+    }
+    @Test
+    public void loginPositiveTestModel(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("tretam0810@gmail.com", "Carro54321#");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertTrue(app.getHelperUser().isLogged());
+
+    }
+
+    @Test
+    public void loginNegativeTestWrongEmail(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("tretam@gmail.com", "Carro54321#");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertFalse(app.getHelperUser().isLogged());
+
+    }
+
+    @Test
+    public void loginNegativeTestWrongPassword(){
+        app.getHelperUser().openLoginForm();
+        app.getHelperUser().fillLoginForm("tretam0810@gmail.com", "Carro543#");
+        app.getHelperUser().submitLogin();
+
+        Assert.assertFalse(app.getHelperUser().isLogged());
 
     }
 }
